ca2cc16580b91aded79f0c3b290b6d6bd5654165
Laura <lazeru08@gmail.com>
2018-12-12 18:53:06 +0100

    Report started + some changes on code

--- 06code.R
+++ 06code.R
@@ -16,15 +16,16 @@ rm(requiredPackages)
 # Set WD and load data
 wd = getwd()
 if(grepl("nora", wd)) {
-    setwd("~/Documents/18-19/IR/LABS/05/")
+    setwd("~/Documents/18-19/CSN/LABS/06/")
 } else {
     # Set Carolina Working directory
-    setwd("~/Documents/FEUP/5A/1S/IR/Information-Retrieval/05")
+    setwd("")
 }
 rm(wd)
 
 ##################
 PLOT = F
+RUN_FROM_SCRATCH = F
 
 simulateSIS <- function(g, beta, gamma, p, ts) {
   
@@ -99,75 +100,105 @@ simulateSIS <- function(g, beta, gamma, p, ts) {
   return(nrInfected)
 }
 
-plotEvolution  <- function(nrInfected, N){
+plotEvolution  <- function(nrInfected, N, title){
     nrSusceptible = N - nrInfected
-    
-    plot(seq(ts),nrSusceptible, type='l', ylim = c(0, N), col="blue")
+    ts = seq(length(nrInfected))
+    plot(ts,nrSusceptible, type='l', ylim = c(0, N), col="blue", main=title, ylab = "# nodes", xlab="t")
     lines(nrInfected, col="red")
+    grid()
+    legend("topright", legend = c("Infected", "Susceptible"),
+           lty = 1, lwd = 2,col = c("red", "blue"))
+}
+
+
+n = 1000
+
+####### Task 1
+if(RUN_FROM_SCRATCH){
+    beta = 0.05
+    gamma = 0.15
+    p = 10
+    ts = 100
+    
+    er.graph <- erdos.renyi.game(n, 0.5)
+    full.graph <- make_full_graph(n, directed=F)
+    ba.graph <-barabasi.game(n, 1, directed=F)
+    star.graph <- make_star(n, mode="undirected")
+    ws.graph <- watts.strogatz.game(1,n,4,0.5)
+    
+    
+    nrInfected.er.graph = simulateSIS(er.graph, beta, gamma, p, ts)
+    nrInfected.full.graph = simulateSIS(full.graph, beta, gamma, p, ts)
+    nrInfected.ba.graph = simulateSIS(ba.graph, beta, gamma, p, ts)
+    nrInfected.star.graph = simulateSIS(star.graph, beta, gamma, p, ts)
+    nrInfected.ws.graph = simulateSIS(ws.graph, beta, gamma, p, ts)
+    
+    
+    if(PLOT){
+      plotEvolution(nrInfected.er.graph, n, "Erdos-Renyi infected/susceptible evolution")
+      plotEvolution(nrInfected.full.graph, n, "Fully connected g. infected/susceptible evolution")
+      plotEvolution(nrInfected.ba.graph, n, "Barabasi-Albert infected/susceptible evolution")
+      plotEvolution(nrInfected.star.graph, n, "Star infected/susceptible evolution")
+      plotEvolution(nrInfected.ws.graph, n, "Watts-Strogatz infected/susceptible evolution")
+    }
+    write.csv2(nrInfected.er.graph, "./data/er_infected.csv")
+    write.csv2(nrInfected.full.graph, "./data/full_infected.csv")
+    write.csv2(nrInfected.ba.graph, "./data/ba_infected.csv")
+    write.csv2(nrInfected.star.graph, "./data/star_infected.csv")
+    write.csv2(nrInfected.ws.graph, "./data/ws_infected.csv")
+} else {
+    nrInfected.er.graph <- read.csv2("./data/er_infected.csv"); nrInfected.er.graph <- nrInfected.er.graph$x
+    nrInfected.full.graph <- read.csv2("./data/full_infected.csv"); nrInfected.full.graph <- nrInfected.full.graph$x
+    nrInfected.ba.graph <- read.csv2("./data/ba_infected.csv"); nrInfected.ba.graph <- nrInfected.ba.graph$x
+    nrInfected.star.graph <- read.csv2("./data/star_infected.csv"); nrInfected.star.graph <- nrInfected.star.graph$x
+    nrInfected.ws.graph <- read.csv2("./data/ws_infected.csv"); nrInfected.ws.graph <- nrInfected.ws.graph$x
+}
+
+if(PLOT) {
+    plot(seq(length(nrInfected.er.graph)), nrInfected.er.graph, type='l', ylim = c(0, n), col="blue", main="Evolutions comparison", ylab = "# infected nodes", xlab="t")
+    lines(nrInfected.full.graph, col="red")
+    lines(nrInfected.ba.graph, col="green")
+    lines(nrInfected.star.graph, col="orchid")
+    lines(nrInfected.ws.graph, col="skyblue")
+    grid()
+    legend("bottomright", legend = c("Erdo-Renyi", "Full graph", "Barabasi-Albert", "Star", "Watts-Strogatz"),
+           lty = 1, lwd = 2,col = c("blue", "red", "green", "orchid", "skyblue"))
+
 }
 
 
 
+# Task 2 Threshold
 
-beta = 0.8
-gamma = 0.4
-p = 5
-
-ts = 30
-n = 1000
-
-er.graph <- erdos.renyi.game(n, 0.5)
-full.graph <- make_full_graph(n, directed=F)
-ba.graph <-barabasi.game(n, 1, directed=F)
-star.graph <- make_star(n, mode="undirected")
-ws.graph <- watts.strogatz.game(1,n,4,0.5)
-
-
-nrInfected.er.graph = simulateSIS(er.graph, beta, gamma, p, ts)
-nrInfected.full.graph = simulateSIS(full.graph, beta, gamma, p, ts)
-nrInfected.ba.graph = simulateSIS(ba.graph, beta, gamma, p, ts)
-nrInfected.star.graph = simulateSIS(star.graph, beta, gamma, p, ts)
-nrInfected.ws.graph = simulateSIS(ws.graph, beta, gamma, p, ts)
-
-
-plotEvolution(nrInfected.er.graph, n)
-plotEvolution(nrInfected.full.graph, n)
-plotEvolution(nrInfected.ba.graph, n)
-plotEvolution(nrInfected.star.graph, n)
-plotEvolution(nrInfected.ws.graph, n)
-
-
-
-
-# Threshold
-
+if(F){
 threshold.er = spectrum(er.graph)$values
 threshold.full = spectrum(full.graph)$values
 threshold.ba = spectrum(ba.graph)$values
 threshold.star = spectrum(star.graph)$values
 threshold.ws = spectrum(ws.graph)$values
 
-beta = 0.005
+beta.2 = 0.005
 #gamma = 0.05
 
-gamma = threshold.er * beta; gamma
+gamma.2 = threshold.er * beta.2; gamma.2
 
-p = 5
+p.2 = 5
+ts.2 = 40
 
-ts = 40
 n = 1000
 
+nrInfected.er.graph.2 = simulateSIS(er.graph, beta.2, gamma.2, p.2, ts.2)
+nrInfected.full.graph.2 = simulateSIS(full.graph, beta.2, gamma.2, p.2, ts.2)
+nrInfected.ba.graph.2 = simulateSIS(ba.graph, beta.2, gamma.2, p.2, ts.2)
+nrInfected.star.graph.2 = simulateSIS(star.graph, beta.2, gamma.2, p.2, ts.2)
+nrInfected.ws.graph.2 = simulateSIS(ws.graph, beta.2, gamma.2, p.2, ts.2)
 
-nrInfected.er.graph = simulateSIS(er.graph, beta, gamma, p, ts)
-nrInfected.full.graph = simulateSIS(full.graph, beta, gamma, p, ts)
-nrInfected.ba.graph = simulateSIS(ba.graph, beta, gamma, p, ts)
-nrInfected.star.graph = simulateSIS(star.graph, beta, gamma, p, ts)
-nrInfected.ws.graph = simulateSIS(ws.graph, beta, gamma, p, ts)
-
-
-plotEvolution(nrInfected.er.graph, n)
-plotEvolution(nrInfected.full.graph, n)
-plotEvolution(nrInfected.ba.graph, n)
-plotEvolution(nrInfected.star.graph, n)
-plotEvolution(nrInfected.ws.graph, n)
+if(PLOT){
+  plotEvolution(nrInfected.er.graph.2, n, "Erdos-Renyi infected/susceptible evolution")
+  plotEvolution(nrInfected.full.graph.2, n, "Fully connected g. infected/susceptible evolution")
+  plotEvolution(nrInfected.ba.graph.2, n, "Barabasi-Albert infected/susceptible evolution")
+  plotEvolution(nrInfected.star.graph.2, n, "Star infected/susceptible evolution")
+  plotEvolution(nrInfected.ws.graph.2, n, "Watts-Strogatz infected/susceptible evolution")
+}
+}
 
